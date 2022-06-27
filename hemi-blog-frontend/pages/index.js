dad6d737dbd95dfd5172aa98dd46ffcddc9dd667
Miktat Cento <themiktat@gmail.com>
2022-06-27 20:08:08 +0300

    x

--- hemi-blog-frontend/pages/index.js
+++ hemi-blog-frontend/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Test from '../components/test'
+import Header from '../components/header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
+      <Header />
 
     </div>
   )
