74495786803e7b56a3cb115826990ed51e40e860
Miktat Cento <themiktat@gmail.com>
2022-06-27 20:51:27 +0300

    x

--- hemi-blog-frontend/components/header.js
+++ hemi-blog-frontend/components/header.js
@@ -1,7 +1,122 @@
-import React from 'react'
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Text,
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { useRouter } from 'next/router'
 
-export default function header() {
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
+
+export default function Header() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const router = useRouter();
+
+  const menuList = [
+    {
+      title: "Anasayfa",
+      path: "/"
+    },
+    {
+      title: "Hakkında",
+      path: "/about"
+    }
+  ]
+
+  const handleRoute = menu => {
+    router.push(menu.path)
+  }
+
   return (
-    <div className='text-2xl'>header</div>
-  )
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems='center' justifyContent='space-between'>
+          <Flex alignItems='center' gap={3}>
+            <Box>
+              <Text fontWeight="bold">
+                HEMİ Blog
+              </Text>
+            </Box>
+
+            {
+              menuList.map(menu => {
+                return (
+                  <Button key={menu.path} onClick={() => handleRoute(menu)} variant={menu.path !== router.pathname ? "ghost" : "solid"}>
+                    {menu.title}
+                  </Button>
+                )
+              })
+            }
+          </Flex>
+
+          <Flex alignItems='center'>
+            <Stack direction='row' spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded='full'
+                  variant='link'
+                  cursor='pointer'
+                  minW={0}>
+                  <Avatar
+                    size={'sm'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                  />
+                </MenuButton>
+
+                <MenuList alignItems='center'>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={'2xl'}
+                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
 }
