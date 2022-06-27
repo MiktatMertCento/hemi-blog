fe77fd3c0e84476c7121e91d666b8f9d61b8aa18
Hasan Oruc <oruch379@gmail.com>
2022-06-27 22:49:44 +0300

    hasan

--- hemi-blog-frontend/pages/about.jsx
+++ hemi-blog-frontend/pages/about.jsx
@@ -1,7 +1,9 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex , Grid, GridItem, Text, Input, InputGroup, InputLeftElement} from '@chakra-ui/react'
+import { PhoneIcon, InfoOutlineIcon, EmailIcon, EditIcon } from '@chakra-ui/icons'
 import Head from 'next/head'
 import React from 'react'
 import Image from 'next/image'
+import AboutStyle from '../styles/About.module.css'
 
 export default function about() {
     return (
@@ -10,10 +12,33 @@ export default function about() {
                 <title>Hakkında</title>
             </Head>
 
-            <>
-                <div className='w-100 h-100 bg-red'>
-                </div>
-            </>
+            <Text display='flex' justifyContent='center' fontSize='3rem' mt='1rem'>Hakkımızda</Text>
+            <Flex p='3rem' justify='center' alignItems='center' gap='6'>
+                <Box width='20rem' height='20rem' backgroundColor='red' ></Box>
+                <Text maxWidth='20rem' fontSize='1.2rem'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae voluptate, asperiores optio quod inventore nulla sit deserunt eaque molestiae consequatur.</Text>
+
+
+            </Flex>
+            <Grid justifyContent='center' mt='5rem' gap='3'>
+                <GridItem><InputGroup w='30rem'><InputLeftElement 
+                pointerEvents='none'
+                children={<InfoOutlineIcon color='white' />} />
+                <Input placeholder='İsminiz' />
+                </InputGroup>
+                </GridItem>
+                <GridItem><InputGroup w='30rem'><InputLeftElement 
+                pointerEvents='none'
+                children={<EmailIcon color='white' />} />
+                <Input placeholder='E-Mailiniz' />
+                </InputGroup>
+                </GridItem>
+                <GridItem><InputGroup w='30rem'><InputLeftElement 
+                pointerEvents='none'
+                children={<EditIcon color='white' />} />
+                <Input placeholder='Mesajınız' h='20rem' />
+                </InputGroup>
+                </GridItem>
+            </Grid>
 
         </>
     )
