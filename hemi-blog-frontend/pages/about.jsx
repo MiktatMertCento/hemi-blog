8e8244a9201275a39b09a5a2c9f7b07a506a8dde
Hasan Oruc <oruch379@gmail.com>
2022-06-30 01:28:30 +0300

    hasan

--- hemi-blog-frontend/pages/about.jsx
+++ hemi-blog-frontend/pages/about.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, GridItem, Text, Input, InputGroup, InputLeftElement, Textarea } from '@chakra-ui/react'
+import { Box, Flex, Grid, GridItem, Text, Input, InputGroup, InputLeftElement, Textarea, Button, Divider } from '@chakra-ui/react'
 import { PhoneIcon, InfoOutlineIcon, EmailIcon, EditIcon } from '@chakra-ui/icons'
 import Head from 'next/head'
 import React from 'react'
@@ -14,11 +14,12 @@ export default function about() {
 
             <Text display='flex' justifyContent='center' fontSize='3rem' mt='1rem'>Hakkımızda</Text>
             <Flex p='3rem' justify='center' alignItems='center' gap='6'>
-                <Box width='20rem' height='20rem' backgroundColor='red' ></Box>
+                <Box width='20rem' height='20rem' backgroundColor='gray' boxShadow='lg' ></Box>
                 <Text maxWidth='20rem' fontSize='1.2rem'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae voluptate, asperiores optio quod inventore nulla sit deserunt eaque molestiae consequatur.</Text>
 
 
             </Flex>
+            <Flex mx='5rem'><Divider/></Flex>
             <Grid justifyContent='center' mt='5rem' gap='3'>
                 <GridItem>
                     <InputGroup w='30rem'>
@@ -43,10 +44,12 @@ export default function about() {
                         <InputLeftElement pointerEvents='none'>
                             <EditIcon color='white' />
                         </InputLeftElement>
-                        <Textarea placeholder='Mesajınız' paddingInlineEnd='1rem' paddingInlineStart='2.5rem' />
+                        <Textarea placeholder='Mesajınız' resize='none' paddingInlineEnd='1rem' paddingInlineStart='2.5rem' maxH='15rem' h='15rem' />
                     </InputGroup>
+                    <Flex mt='0.75rem' justifyContent='flex-end' ><Button>Gönder</Button></Flex>
                 </GridItem>
             </Grid>
+            <Box w='100%' h='10rem' ></Box>
 
         </>
     )
