0f08d02a6101bb3801a9d94b113cc78a37c4e6a9
Miktat Cento <themiktat@gmail.com>
2022-07-03 16:05:45 +0300

    x

--- hemi-blog-frontend/components/post.jsx
+++ hemi-blog-frontend/components/post.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Text } from "@chakra-ui/react";
+import { Box, Flex, Stack, Text } from "@chakra-ui/react";
 
 export default function PostComponent({ postInfo }) {
     return (
@@ -6,11 +6,11 @@ export default function PostComponent({ postInfo }) {
             <Box pos="absolute" top="0" right="0" left="0" bottom="0" bgSize="cover" bgPos="center" filter="blur(.1px)" bgImage="url(https://upload.wikimedia.org/wikipedia/tr/d/d3/Windows_11_Yeni_Ekran_Görüntüsü.jpg)" borderRadius="lg" />
             <Box pos="absolute" top="0" right="0" left="0" bottom="0" bgGradient='linear-gradient(to bottom, transparent 0%, black 100%)' opacity=".9" borderRadius="lg" />
             <Box pos="absolute" left="0" bottom="0" padding="16px">
-                <Box>
-                    <Text color="white" bgColor="red">Teknoloji</Text>
-                </Box>
-                <Text color="white" fontWeight="bold">{postInfo.title}</Text>
-                <Text color="white" >Miktat Cento</Text>
+                <Flex flexDir="column">
+                    <Text alignSelf="start" color="white" bgColor="red" fontWeight="bold" fontSize=".85rem" px={2} py={.5}>Teknoloji</Text>
+                    <Text alignSelf="start" color="white" fontWeight="bold">{postInfo.title}</Text>
+                    <Text alignSelf="start" color="white" >Miktat Cento</Text>
+                </Flex>
             </Box>
         </Box>
     )
